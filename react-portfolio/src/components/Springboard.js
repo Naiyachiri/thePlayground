f6a3782fe96fc2026901712aeb1868e87aff5646
James Bui <buijvbio@gmail.com>
2018-11-15 12:02:11 -0500

    FEAT: hooked up react-router-dom
    initialized springboard gallery as '/'

---
+++ react-portfolio/src/components/Springboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Springboard extends Component {
+  render() {
+    return (
+      <div className="springboard">
+        springboard photo gallery
+      </div>
+    );
+  }
+}
+
+export default Springboard;
