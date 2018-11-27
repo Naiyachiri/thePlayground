2cdd5d9de6d6e9acc86911ffa415cf9b99a6fa05
James Bui <buijvbio@gmail.com>
2018-11-27 10:28:41 -0500

    FEAT: added tic tac toe game preview

--- react-portfolio/src/components/Springboard.js
+++ react-portfolio/src/components/Springboard.js
@@ -21,12 +21,17 @@ class Springboard extends Component {
             <span>buijvbio@gmail.com</span>
           </div>
         </div>
-        <a href='https://codepen.io/naiyachiri/full/jarQpa/' rel='noopener noreferrer' id='js-calc-link'>
+        <a href='https://codepen.io/naiyachiri/full/jarQpa/' rel='noopener noreferrer' id='js-calc-link' data-tool-tip='This is a javascript based calculator that performs the basic four functions in addition to some advanced functions. It uses jQuery, javascript, and bootstrap' target='_blank'>
           <div className='cell cell1' id='js-calculator-cell'>
             <p>JS Calculator</p>
           </div>
         </a>
-        <div className='cell cell1'>cell 4</div>
+        <a href='https://codepen.io/naiyachiri/full/bYaPOB/' rel='noopener noreferrer' id='tic-tac-toe-link' data-tool-tip='This is a tic tac toe game implementation using jQuery, javascript, and bootstrap.' target='_blank'>
+          <div className='cell cell1' id='tic-tac-toe-cell'>
+            <p>Tic Tac Toe Game</p>
+          </div>
+        </a>
+        <div className='cell cell1'>Tic-Tac-Toe</div>
         <div className='cell cell4'>cell 5, 6, 7, 8</div>
         <div className='cell cell1'>cell 9</div>
         <div className='cell cell3'>cell 10, 11, 12</div>
