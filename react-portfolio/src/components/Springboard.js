752e99612b5e792363397d6dee71e50d2d2bdb19
James Bui <buijvbio@gmail.com>
2018-12-05 10:16:00 -0500

    CHORE: refactored css classes
    cleaned up cell formats => .cell-header, .cell-body, .cell-footer
    
    take up 20%, 60%, and 20% of cell space.

--- react-portfolio/src/components/Springboard.js
+++ react-portfolio/src/components/Springboard.js
@@ -6,50 +6,51 @@ class Springboard extends Component {
     return (
       <section className="grid">
         <div className='cell cell2' id='profile'>
-          <div className='cell-text cell-text-header'>
-            Profile
-          </div>
-          <a href='https://github.com/Naiyachiri/thePlayground' className='cell-text'>
-          <img className='cell-image' src={require('../img/GitHub-Mark-64px.png')} alt='github link'/>
-          <p className='cell-text'>GitHub</p>
-          </a>
-          <p className='cell-blog-content'>
-          Hi there! I am a recently graduated front-end web developer looking for new opportunities! I have a nanodegree from
-          <a className='article-link' id='udacity-degree-link' data-tool-tip='Google and AT&T backed coding nanodegree which included over 1200 hours of intensive deadline-based self study. Material covered included HTML5, CSS, ES6 JS, frameworks such as React, Angular, Ember with dedicated projects in React.' href='https://confirm.udacity.com/PVJXEUHH'>
-            Udacity
-          </a>
-          and a certification from
-          <a className='article-link' id='fcc-cert-link' data-tool-tip='12 Front end projects (Simon Says game, calculator, wiki search engine) covering HTML, CSS and over 400 hours of pure javascript algorithms and data structures practice.' href='https://www.freecodecamp.org/naiyachiri/front-end-certification'>
-            FreeCodeCamp
-          </a>.
-          </p>
-          <a href='#' className='cell-text'>
-            <img className='cell-image' src={require('../img/In-2C-75px-R.png')} alt='linkedin url'/>
-          </a>
-          <div className='cell-text cell-text-footer'>
+          <div className='cell-text cell-header'>
             <span>James Bui</span>
             <span>757.746.8618</span>
             <span>buijvbio@gmail.com</span>
           </div>
+          <div className='cell-body'>
+            Hi there! I am a recently graduated front-end web developer looking for new opportunities! I have a nanodegree from
+              <a className='inline-link' id='udacity-degree-link' data-tool-tip='Google and AT&T backed coding nanodegree which included over 1200 hours of intensive deadline-based self study. Material covered included HTML5, CSS, ES6 JS, frameworks such as React, Angular, Ember with dedicated projects in React.' href='https://confirm.udacity.com/PVJXEUHH'>
+                Udacity
+              </a>
+            and a certification from
+              <a className='inline-link' id='fcc-cert-link' data-tool-tip='12 Front end projects (Simon Says game, calculator, wiki search engine) covering HTML, CSS and over 400 hours of pure javascript algorithms and data structures practice.' href='https://www.freecodecamp.org/naiyachiri/front-end-certification'>
+              FreeCodeCamp
+            </a>.
+          </div>
+          <div className='cell-text cell-footer'>
+            <a href='https://www.linkedin.com/in/buijames' target='_blank' rel='noopener noreferrer' id='linkedin-link'>
+              Linkedin
+            </a>
+            <a href='https://github.com/Naiyachiri/thePlayground'>
+              GitHub
+            </a>
+            <a href='https://drive.google.com/open?id=14wLbcDQbXjqnTSxmApYWtM0KcckNJQ_g4OXqIe7TJ-0' target='_blank' rel='noopener noreferrer'>
+              Resume
+            </a>
+          </div>
         </div>
         <a className='project-link' id='js-calc-link' href='https://codepen.io/naiyachiri/full/jarQpa/' rel='noopener noreferrer'  data-tool-tip='This is calculator that performs the basic four functions in addition to some advanced functions. It uses jQuery, javascript, and Bootstrap.' target='_blank'>
           <div className='cell cell1' id='js-calculator-cell'>
-            <div className='cell-text cell-text-header'>JS Calculator</div>
+            <div className='cell-text cell-header'>JS Calculator</div>
           </div>
         </a>
         <a className='project-link' id='tic-tac-toe-link' href='https://codepen.io/naiyachiri/full/bYaPOB/' rel='noopener noreferrer'  data-tool-tip='This is a tic tac toe game implementation using jQuery, javascript, and Bootstrap.' target='_blank'>
           <div className='cell cell1' id='tic-tac-toe-cell'>
-            <div className='cell-text cell-text-header'>Tic Tac Toe Game</div>
+            <div className='cell-text cell-header'>Tic Tac Toe Game</div>
           </div>
         </a>
         <a className='project-link' id='neighborhood-map-link' href='http://naiyachiri.github.io/fend-neighborhood-map' rel='noopener noreferrer'  data-tool-tip='React based google API powered application that tracks coffee shops in the Springfield, VA area. It uses Google and Foursquare to search, and dynamically generate info windows for the user.' target='_blank'>
           <div className='cell cell4' id='neighborhood-map-cell'>
-            <div className='cell-text cell-text-footer'>
+            <div className='cell-text cell-footer'>
         Northern Virginia Coffee Tracker</div>
           </div>
         </a>
         <div className='cell cell1 cell-blog'>
-          <div className='cell-text cell-text-header'>
+          <div className='cell-text cell-header'>
             <h3>Recent Activity: </h3>
           </div>
           <div className='cell-blog-content'>
