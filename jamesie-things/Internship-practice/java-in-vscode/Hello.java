fe505fe8fc953878553d28d83199d1dacfb2adde
James Bui <buijv@James-MBP.local>
2019-02-26 14:48:23 -0500

    FEAT: Java, Maven, tomcat - learned basic java (java-in-vscode) - learned basic maven build (jersey-mvn, maven-project) - built basic webapp that takes get request (jersey-webapp, build-restful-api) - hosted maven generated war package on tomcat server

---
+++ jamesie-things/Internship-practice/java-in-vscode/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Hello
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        String test = "Testing! its time to make it rain progress!";
+        System.out.println( test);
+    }
+}
